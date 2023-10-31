22320550ed5c10f20d4a0d2513a47b22d3ebf3b8
IlyaBulatau <ilyabulatau@gmail.com>
2023-10-31 21:16:48 +0200

    Add decode_respocss to redis client object

--- engbot/services/cache/storage.py
+++ engbot/services/cache/storage.py
@@ -3,4 +3,4 @@ from redis import Redis
 from engbot.config import Config
 
 
-redis_cli = Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+redis_cli = Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
