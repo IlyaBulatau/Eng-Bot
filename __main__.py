5d2f8a6716d5e8452bd54e0281f29db8b9562512
IlyaBulatau <ilyabulatau@gmail.com>
2023-10-31 18:59:02 +0200

    Mod __main__.py file
    Now application obj - the ApplicationBuilder instance
    builder object - application object, added to application instance post_init method
    for creating somethings before start bot

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import logging
 
 from engbot.handlers.setup_handlers import setup_handlers
 from engbot.config import Config
+from engbot.utils.set_command import set_command_ui
 
 
 logging.basicConfig(
@@ -12,8 +13,9 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(token=Config.BOT_TOKEN).concurrent_updates(False).build()
-
-    setup_handlers(application)
-
-    application.run_polling()
+    application = ApplicationBuilder().token(token=Config.BOT_TOKEN)
+    builder = application.concurrent_updates(False).post_init(set_command_ui).build()
+    
+    setup_handlers(builder)
+    
+    builder.run_polling()
