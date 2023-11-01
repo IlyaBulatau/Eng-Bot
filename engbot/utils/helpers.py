87697e52908d290b0732f93071f46fe143dbb146
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-01 14:59:13 +0200

    Mod utils/ folder
    Add Enum class CommandEnum that containe all commands

--- engbot/utils/helpers.py
+++ engbot/utils/helpers.py
@@ -5,7 +5,7 @@ def convert_date_to_string(date: date) -> str:
     """
     Convert date object to string object
     """
-    _format = "%-d.%m.%Y"
+    _format = "%-d%m%Y"
     result: str = date.strftime(_format)
-    
-    return result
+
+    return "d" + result
