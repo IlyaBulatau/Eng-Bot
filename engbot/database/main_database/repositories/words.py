d637d4147c7b173c15f357efaeab58713647d24f
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-01 12:40:32 +0200

    Add main_database/repositories/words.py file
    The File need for perform CRUD operations with Word object

---
+++ engbot/database/main_database/repositories/words.py
@@ -0,0 +1,15 @@
+from engbot.database.main_database.repositories.base import CreateBase, DetailBase
+from engbot.models.users import User
+from engbot.models.words import Word, DateCreated
+
+
+class DetailWord:
+    ...
+
+
+class ListWord:
+    ...
+
+
+class CreateWord(CreateBase):
+    ...
