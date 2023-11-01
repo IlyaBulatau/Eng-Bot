2a7d7bc36e214f4626013c38fd0e80c47fd260ae
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-01 15:24:18 +0200

    Mod models/users.py
    Add Enum class
    AND
    Create additions between User class and EnumUser class

--- engbot/models/users.py
+++ engbot/models/users.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, validator
 from engbot.models.words import DateCreated
 
+from enum import Enum
+
 
 class User(BaseModel):
     """
@@ -12,6 +14,19 @@ class User(BaseModel):
     language_code: str
     words: DateCreated | None = None
 
+    @validator("telegram_id", "username", "language_code")
+    def validate_all_fields(cls, kwargs):
+        """
+        Enum class in this module must match with this class
+        """
+        self_fields = set(User.model_fields.keys())
+        enum_fields = set(elem.value for elem in tuple(UserField))
+        
+        if len(self_fields.intersection(enum_fields)) != len(enum_fields):
+            raise Exception("Fileds in model and enum class must match")
+        return kwargs
+        
+
     @validator("telegram_id")
     def telegram_id_validate(cls, telegram_id: int | str) -> str:
         if isinstance(telegram_id, int):
@@ -22,3 +37,10 @@ class User(BaseModel):
             raise Exception(
                 "Telegram id must be is integer or is a string contain only integer"
             )
+
+
+class UserField(Enum):
+    TELEGRAM_ID: str = "telegram_id"
+    USERNAME: str = "username"
+    LANGUAGE_CODE: str = "language_code"
+    WORDS: str =  "words"
