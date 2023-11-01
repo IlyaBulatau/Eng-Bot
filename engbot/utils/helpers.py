beb9609fbcd514fd1abc0c7f6690e168eea49e38
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-01 15:37:02 +0200

    Mod utils/helper.py file
    Out func for intersection in this file

--- engbot/utils/helpers.py
+++ engbot/utils/helpers.py
@@ -1,4 +1,6 @@
 from datetime import date
+from pydantic import BaseModel
+from enum import Enum
 
 
 def convert_date_to_string(date: date) -> str:
@@ -9,3 +11,15 @@ def convert_date_to_string(date: date) -> str:
     result: str = date.strftime(_format)
 
     return "d" + result
+
+
+def intersection_model_and_enum(obj_model: BaseModel, obj_enum: Enum):
+        """
+        Enum class must match with model class
+        """
+
+        self_fields = set(obj_model.model_fields.keys())
+        enum_fields = set(elem.value for elem in tuple(obj_enum))
+        
+        if len(self_fields.intersection(enum_fields)) != len(enum_fields):
+            raise Exception(f"Fileds in model and enum class must match\n\nModel: {obj_model.__name__}, Enum: {obj_enum.__name__}")
