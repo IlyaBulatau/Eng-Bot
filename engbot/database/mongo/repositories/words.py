621b5af3084e3442a559d7b458540806db279a44
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-03 15:42:01 +0200

    Mod mongo/repositories/words.py
    Update get_all_words_of_user function
    Now it return list of WordList object with reverse by date

--- engbot/database/mongo/repositories/words.py
+++ engbot/database/mongo/repositories/words.py
@@ -3,9 +3,10 @@ from pymongo.collection import Collection
 from datetime import date
 
 from engbot.utils.helpers import convert_date_to_string
-from engbot.models.words import WordList, Word, WordListField
+from engbot.models.words import WordList, Word, WordListField, WordField
 from engbot.models.users import UserField
 from engbot.database.mongo.repositories.users import get_user_by_argument
+from engbot.utils.helpers import reform_from_string_to_string_of_date
 
 
 def create_word_of_user(
@@ -49,5 +50,24 @@ def get_all_words_of_user(
     user_dict: dict = get_user_by_argument(
         collection=collection, telegram_id=str(telegram_id)
     )
+    list_words_in_date: list[
+        dict[str, dict[str, list[dict[str, str]]]]
+    ] = user_dict.get(UserField.WORDS.value)
 
-    return user_dict
+    words: list[WordList] = [
+        WordList(
+            created_on=reform_from_string_to_string_of_date(date),
+            words=[
+                Word( # create words objects
+                    eng_word=translate_dict.get(WordField.ENG_WORD.value),
+                    translate=translate_dict.get(WordField.TRANSlATE.value),
+                )
+                for translate_dict in dict_word # getting every set of words by 'this' date
+            ],
+        )
+        for dates in list_words_in_date[::-1] # get WordListField.CREATED_ON.value in reverse list
+        for dict_date_in_word in dates.values() # getting dict with date - key and set of wod - value 
+        for date, dict_word in dict_date_in_word.items() # this key valye
+    ]
+
+    return words
