35946e8d300d4892f84e99b6368e41331abee409
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-03 19:00:07 +0200

    Mod keyboards.py file
    Add docs to func
    AND
    Created func which creating low arrows menu for other keyboards

--- engbot/utils/keyboards.py
+++ engbot/utils/keyboards.py
@@ -1,6 +1,12 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from engbot.models.words import Word, WordList, WordField
+from engbot.models.words import WordList, WordField
+from engbot.utils.callback_datas import (
+    LEFT_BUTTOM,
+    RIGHT_BUTTOM,
+    ENGLISH_LAGUAGE,
+    RUSSIAN_LANGUAGE,
+)
 
 
 def keyboard_of_words(
@@ -8,15 +14,44 @@ def keyboard_of_words(
 ) -> InlineKeyboardMarkup:
     """
     Create keyboard for show words
+
+    Arguments:
+    :words_list - list of WordList object that containe
+                  all words of user from database
+    :limit -      How many words shows in keyboard
+    :offset -     which date with words will showing, starting from the end array
     """
-    
+
     markup: list[list[InlineKeyboardButton]] = [
-        [InlineKeyboardButton(
-            text=word_object.model_dump().get(WordField.ENG_WORD.value),
-            callback_data=words_list[offset].created_on
-        )]
+        [
+            InlineKeyboardButton(
+                text=word_object.model_dump().get(WordField.ENG_WORD.value),
+                callback_data=words_list[offset].created_on,
+            )
+        ]
         for word_object in words_list[offset].words
     ][:limit]
-    
+
+    arrows_bar = create_low_arrows_bar_to_keyboards()
+    markup.extend(arrows_bar)
+
     return InlineKeyboardMarkup(markup)
 
+
+def create_low_arrows_bar_to_keyboards(
+    left_arrows=True, right_arraow=True, language_turn=RUSSIAN_LANGUAGE
+) -> list[InlineKeyboardButton]:
+    markup = []
+    middle_text = "🇬🇧" if language_turn == ENGLISH_LAGUAGE else "🇷🇺"
+
+    left_buttom = InlineKeyboardButton(text="<<", callback_data=LEFT_BUTTOM)
+    right_buttom = InlineKeyboardButton(text=">>", callback_data=RIGHT_BUTTOM)
+    midle_buttom = InlineKeyboardButton(text=middle_text, callback_data=language_turn)
+
+    if left_arrows:
+        markup.append(left_buttom)
+    markup.append(midle_buttom)
+    if right_arraow:
+        markup.append(right_buttom)
+
+    return [markup]
