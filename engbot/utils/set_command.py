d76c1bf5b9814cdf5aeead938d26f21f0aef533d
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-03 14:10:45 +0200

    Mod utils.set_commands.py file
    Add new command 'words' for show all words of user

--- engbot/utils/set_command.py
+++ engbot/utils/set_command.py
@@ -8,6 +8,7 @@ class CommandEnum(Enum):
     START: str = "start"
     CANCEL: str = "cancel"
     NEW: str = "new"
+    WORDS: str = "words"
 
 
 async def set_command_ui(application: Application) -> None:
@@ -21,6 +22,7 @@ async def set_command_ui(application: Application) -> None:
         BotCommand(command=CommandEnum.START.value, description="Начало"),
         BotCommand(command=CommandEnum.CANCEL.value, description="Сброс"),
         BotCommand(command=CommandEnum.NEW.value, description="Новое слово"),
+        BotCommand(command=CommandEnum.WORDS.value, description="Мои слова"),
     ]
 
     await bot.set_my_commands(commands=commands)
