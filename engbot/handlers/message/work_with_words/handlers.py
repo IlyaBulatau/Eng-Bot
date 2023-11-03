e21e5459aece960538e8bab1088982563fd0de86
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-03 21:50:59 +0200

    Create handlers/message/work_with_words/handlers.py file
    Create handler that reacts on touch arrows on arrrows-bar menu under words list

--- engbot/handlers/message/work_with_words/handlers.py
+++ engbot/handlers/message/work_with_words/handlers.py
@@ -1,7 +1,42 @@
-from telegram.ext import (
-    ContextTypes,
-    ConversationHandler,
-    MessageHandler,
-    CommandHandler,
-)
-from telegram import Update
+from telegram.ext import ContextTypes
+from telegram import Update, CallbackQuery
+
+from engbot.services.cache.states import CahceCurrentUserPage
+from engbot.utils.keyboards import keyboard_of_words
+from engbot.database.main_database.repositories.words import ListWord
+from engbot.models.words import WordList
+from engbot.utils.callback_datas import LEFT_BUTTOM, RIGHT_BUTTOM
+
+
+async def callback_arrows(update: Update, context: ContextTypes):
+    """
+    Reacts on touch arrows buttom
+
+    If user touch left button - decrease current page if it touch right increment
+    """
+    # callback datas
+    callback: CallbackQuery = update.callback_query
+    callback_data = callback.data
+    user_telegram_id: int = callback.from_user.id
+
+    # get words
+    get_words = ListWord(user_telegram_id)
+    words: list[WordList] = get_words()
+
+    # update current page in cache
+    cache = CahceCurrentUserPage(user_telegram_id=user_telegram_id)
+
+    if callback_data == LEFT_BUTTOM:
+        cache.update_page(amount=1, decrease=False)
+    elif callback_data == RIGHT_BUTTOM:
+        cache.update_page(amount=1)
+
+    current_page: int = cache.get_current_page()
+
+    # build answer
+    date_created_words: str = words[current_page].created_on
+    markup = keyboard_of_words(words_list=words, offset=current_page)
+
+    await callback.edit_message_text(
+        text=f"Дата добавления: {date_created_words}", reply_markup=markup
+    )
--- engbot/handlers/setup_handlers.py
+++ engbot/handlers/setup_handlers.py
@@ -1,9 +1,17 @@
 from engbot.handlers.commands.assemble import assemble_commands
 from engbot.handlers.message.create_new_word.handlers import HANDLER_CREATE_NEW_WORD
+from engbot.handlers.message.work_with_words import handlers
+from engbot.utils.callback_datas import LEFT_BUTTOM, RIGHT_BUTTOM
+from engbot.utils.helpers import accept_callback_arrows
 
-from telegram.ext import Application
+from telegram.ext import Application, CallbackQueryHandler
 
 
 def setup_handlers(application: Application):
     application.add_handler(HANDLER_CREATE_NEW_WORD)
     assemble_commands(application)
+    application.add_handler(
+        CallbackQueryHandler(
+            callback=handlers.callback_arrows, pattern=accept_callback_arrows
+        )
+    )
