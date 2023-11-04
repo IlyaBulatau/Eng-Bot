2d8f1f8b28f96821e2ca7daa3f35b2cd047cdbd9
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-04 19:22:02 +0200

    Create services/limiters.py file
    Create in this file class for tracking quantity words os user

---
+++ engbot/services/limiters.py
@@ -0,0 +1,26 @@
+from engbot.database.main_database.db import Database
+from engbot.database.mongo.repositories.words import count_words_of_day_in_array
+
+
+class WordLimiter:
+    """
+    Object for tracking limiting quantity words in day
+
+    User don't can create more than QUANTITY_IN_DAY words in day
+    """
+
+    QUANTITY_IN_DAY = 10
+
+    def __init__(self):
+        self.__connection_to_db = Database().get_connection()
+
+    def is_acceptable(self, telegram_id: str | int):
+        """
+        Cheking the word count
+        """
+        qiantity: int = count_words_of_day_in_array(
+            collection=self.__connection_to_db, telegram_id=telegram_id
+        )
+        if qiantity and qiantity == self.QUANTITY_IN_DAY:
+            return False
+        return True
