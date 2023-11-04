9057a2d4627d64f55fb6e6f5632213a43fb60864
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-04 19:19:59 +0200

    Mod mongo.repositories/words.py
    Create new func for getting quantity of words in today for definite user

--- engbot/database/mongo/repositories/words.py
+++ engbot/database/mongo/repositories/words.py
@@ -2,11 +2,14 @@ from pymongo.collection import Collection
 
 from datetime import date
 
-from engbot.utils.helpers import convert_date_to_string
+from engbot.utils.helpers import (
+    convert_date_to_string,
+    reform_from_string_to_string_of_date,
+)
 from engbot.models.words import WordList, Word, WordListField, WordField
 from engbot.models.users import UserField
 from engbot.database.mongo.repositories.users import get_user_by_argument
-from engbot.utils.helpers import reform_from_string_to_string_of_date
+from engbot.database.mongo.mongodb import MONGO_ID_FIELD
 
 
 def create_word_of_user(
@@ -81,3 +84,57 @@ def get_all_words_of_user(
     ]
 
     return words
+
+
+def count_words_of_day_in_array(
+    collection: Collection, telegram_id: str | int
+) -> int | None:
+    """
+    Getting count words of current day of user from mongodb
+
+    Process:
+    In first - getting data by telegram ID and date today,
+    then keeping only field 'words',
+    furth created dict with key of 'words' nad value of list of list of words,
+    and finally remove inner list and getting size of list
+    """
+    created_on_key: str = convert_date_to_string(date.today())
+
+    pipline = [
+        {  # get data by telegram ID
+            "$match": {UserField.TELEGRAM_ID.value: str(telegram_id)},
+        },
+        {  # get data by date today
+            "$match": {
+                f"{UserField.WORDS.value}.{WordListField.CREATED_ON.value}.{created_on_key}": {
+                    "$exists": 1
+                },
+            }
+        },  # get only field of words
+        {"$project": {UserField.WORDS.value: 1, MONGO_ID_FIELD: 0}},
+        {  # create dict with key 'words' and value list of words
+            "$group": {
+                MONGO_ID_FIELD: 0,
+                UserField.WORDS.value: {
+                    "$first": f"${UserField.WORDS.value}.{WordListField.CREATED_ON.value}.{created_on_key}"
+                },
+            }
+        },
+        {  # get first element from list of words
+            "$project": {
+                MONGO_ID_FIELD: 0,
+                f"{UserField.WORDS.value}": {"$first": f"${UserField.WORDS.value}"},
+            }
+        },
+        {  # get size of list
+            "$project": {
+                MONGO_ID_FIELD: 0,
+                f"{UserField.WORDS.value}": {"$size": f"${UserField.WORDS.value}"},
+            }
+        },
+    ]
+
+    data_from_mongo = collection.aggregate(pipline)
+    dict_result: dict = [dicts for dicts in data_from_mongo][0]
+    count: str | None = dict_result.get(UserField.WORDS.value, None)
+    return int(count) if count else None
