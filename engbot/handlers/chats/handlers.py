6013a59e1ccdbab1465f5408667a034d2df39465
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-06 13:37:26 +0200

    Add ChatMemberHanlder to setup_handler file

---
+++ engbot/handlers/chats/handlers.py
@@ -0,0 +1,7 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+async def track_chats(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ...
+
--- engbot/handlers/setup_handlers.py
+++ engbot/handlers/setup_handlers.py
@@ -1,12 +1,18 @@
 from engbot.handlers.commands.assemble import assemble_commands
 from engbot.handlers.message.create_new_word.handlers import HANDLER_CREATE_NEW_WORD
 from engbot.handlers.message.work_with_words import handlers
+from engbot.handlers.chats.handlers import track_chats
 from engbot.utils.helpers import accept_callback_arrows, accept_callback_language_type
 
-from telegram.ext import Application, CallbackQueryHandler
+from telegram.ext import Application, CallbackQueryHandler, ChatMemberHandler
 
 
 def setup_handlers(application: Application):
+    application.add_handler(
+        ChatMemberHandler(
+            callback=track_chats, chat_member_types=ChatMemberHandler.ANY_CHAT_MEMBER
+        )
+    )
     application.add_handler(HANDLER_CREATE_NEW_WORD)
     assemble_commands(application)
     application.add_handler(
