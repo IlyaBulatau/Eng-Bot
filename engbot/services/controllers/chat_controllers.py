13dcf95f58a4204cde8c7479a78234ac79d1785a
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-06 18:27:03 +0200

    Create ChatController that tracking that user is member group

--- engbot/services/controllers/chat_controllers.py
+++ engbot/services/controllers/chat_controllers.py
@@ -1,8 +1,31 @@
 from engbot.config import Config
+from engbot.services.cache.states import CacheBotGroup
 
-from telegram.ext import ContextTypes
-from telegram import ChatMemberUpdated, Update
+from telegram.ext import ExtBot
+from telegram import Update, Chat
+from telegram.constants import ChatMemberStatus as CS
 
 
 class ChatController:
-    ...
+    """
+    Tracking that user is member group
+    """
+    def __init__(self, update: Update, bot: ExtBot):
+        self.update: Update = update
+        self.bot: ExtBot = bot
+        self.user_id: str = str(self.update.effective_user.id)
+
+    async def control(self):
+        # getting all groups of the bot from cache
+        cache = CacheBotGroup(self.update)
+        groups: list[str] | list = cache.get_groups()
+
+        result = []
+        for group_id in groups:
+            event = await self.bot.get_chat_member(chat_id=group_id, user_id=self.user_id)
+            if event.status not in (CS.ADMINISTRATOR, CS.MEMBER, CS.OWNER):
+                chat: Chat = await self.bot.get_chat(chat_id=group_id)
+                result.append(chat.invite_link)
+
+        return result
+            
