6f203465bb49bb8b2a53c1d1976a963b134d14c5
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-06 16:42:25 +0200

    Refactoring the code

--- engbot/handlers/chats/handlers.py
+++ engbot/handlers/chats/handlers.py
@@ -11,26 +11,31 @@ async def track_my_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     If bot stating is admin the group
     this  will write in cache
-    
+
     If bot kicked from group or not be is not admin
     this also will writing in cache
     """
     event: ChatMemberUpdated = update.my_chat_member
     status: str = event.new_chat_member.status
-    is_admin: bool = True if status in (ChatMember.ADMINISTRATOR, ChatMember.OWNER) else None
-    
+    is_admin: bool = (
+        True if status in (ChatMember.ADMINISTRATOR, ChatMember.OWNER) else None
+    )
+
     group_id = event.chat.id
 
     cache = CacheBotGroup(update)
     if is_admin:
         cache.set_group(group_id)
     else:
-        cache.remove_group(group_id) 
+        cache.remove_group(group_id)
 
 
 async def track_chats_memer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
-    Trcking adding and lick or leaving user from group where thw bot is an admin
+    Trcking adding and lick or leaving user from group where the bot is an admin
     """
-    event = update.chat_member
+    ...
+
+
+
     
--- engbot/services/cache/states.py
+++ engbot/services/cache/states.py
@@ -13,7 +13,9 @@ class State(BaseStorage):
             raise Exception("update object must be Update instance")
         self.update: Update = update
         self.user_telegram_id: str = str(self.update.effective_user.id)
-        self.__cache_key: str = self.CACHE_PREFIX + self.CACHE_SEP + self.user_telegram_id
+        self.__cache_key: str = (
+            self.CACHE_PREFIX + self.CACHE_SEP + self.user_telegram_id
+        )
 
     @property
     def cache_key(self):
@@ -41,7 +43,9 @@ class CahceCurrentUserPage(BaseStorage):
     def __init__(self, user_telegram_id: int | str):
         super().__init__()
         self.user_telegram_id = str(user_telegram_id)
-        self.__cache_key: str = self.CACHE_PREFIX + self.CACHE_SEP + self.user_telegram_id
+        self.__cache_key: str = (
+            self.CACHE_PREFIX + self.CACHE_SEP + self.user_telegram_id
+        )
         self.__start_page: int = 0
         self.ttl_sec: int = 2419200  # 1 mounth
 
@@ -93,13 +97,11 @@ class CacheBotGroup(BaseStorage):
         super().__init__()
         self.update: Update = update
         self.user_telegram_id: str = str(self.update.effective_user.id)
-        self.__cache_key: str = self.CACHE_PREFIX + self.CACHE_SEP + self.user_telegram_id
-
+        self.__cache_key: str = self.CACHE_PREFIX
 
     @property
     def cache_key(self):
         return self.__cache_key
-
 
     def get_groups(self) -> list[str] | list:
         """
@@ -109,44 +111,42 @@ class CacheBotGroup(BaseStorage):
         str_of_groups_list = self.storage.get(self.cache_key)
         if not str_of_groups_list:
             return []
-        
+
         list_of_group: list = self._groups_to_list(str_of_groups_list)
-        
+
         if not list_of_group:
             return []
-        
+
         return list_of_group
-        
 
     def set_group(self, telegram_group_id) -> None:
         """
         Added new group for bot
         """
         groups: list[str] | list = self.get_groups()
-        
+
         if str(telegram_group_id) in groups:
             return
-        
+
         groups.append(str(telegram_group_id))
 
         save_obj: str = self._groups_to_str(groups)
         self.storage.set(self.__cache_key, save_obj)
 
-        
     def remove_group(self, telegram_group_id) -> None:
         """
         Removed group for bot
         """
         groups: list[str] | list = self.get_groups()
-        
+
         if not str(telegram_group_id) in groups:
             return
-        
-        groups.remove(str(telegram_group_id)) 
-        
+
+        groups.remove(str(telegram_group_id))
+
         save_obj: str = self._groups_to_str(groups)
         self.storage.set(self.__cache_key, save_obj)
-        
+
     def _groups_to_str(self, list_of_str: list[str]) -> str:
         """
         Receive list of str
@@ -154,17 +154,17 @@ class CacheBotGroup(BaseStorage):
         Example: [1, 2, 3] => '[1, 2, 3]'
         """
         return str(list_of_str)
-    
+
     def _groups_to_list(self, string: str) -> list[str] | list:
         """
         Getting a string as a list of strings
         Return list of strings
         Example: '[1, 2, 3]' => [1, 2, 3]
         """
-        new_string: str = string[1:-1] # remove []
+        new_string: str = string[1:-1]  # remove []
         if new_string.strip() == "":
             return []
         list_of_str: list[str] = new_string.split(", ")
-        result: list[str] = [item[1:-1] for item in list_of_str] # remove quotes
-        
+        result: list[str] = [item[1:-1] for item in list_of_str]  # remove quotes
+
         return result
