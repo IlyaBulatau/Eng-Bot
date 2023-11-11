26966df372870abbb79c4e2ce0caa9754491bc4d
IlyaBulatau <ilyabulatau@gmail.com>
2023-11-11 18:47:02 +0200

    Mod set_command.py file
    Add 2 command /info /translate

--- engbot/utils/set_command.py
+++ engbot/utils/set_command.py
@@ -9,6 +9,8 @@ class CommandEnum(Enum):
     CANCEL: str = "cancel"
     NEW: str = "new"
     WORDS: str = "words"
+    TRANSLATE: str = "translate"
+    INFO: str = "info"
 
 
 async def set_command_ui(application: Application) -> None:
@@ -23,6 +25,8 @@ async def set_command_ui(application: Application) -> None:
         BotCommand(command=CommandEnum.CANCEL.value, description="Сброс"),
         BotCommand(command=CommandEnum.NEW.value, description="Новое слово"),
         BotCommand(command=CommandEnum.WORDS.value, description="Мои слова"),
+        BotCommand(command=CommandEnum.TRANSLATE.value, description="Перевод слова"),
+        BotCommand(command=CommandEnum.INFO.value, description="Описание команд"),
     ]
 
     await bot.set_my_commands(commands=commands)
